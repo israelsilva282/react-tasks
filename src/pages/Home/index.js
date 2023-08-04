7e6b3a0d1dbffa50b2cb4f61d0a0be12463c3d5f
israelsilva282 <israelcostas@hotmail.com>
2023-08-04 15:40:13 -0300

    criado página de login

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './home.css';
 
 function Home() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleLogin(e) {
+        e.preventDefault();
+
+        if (email !== '' && password !== '') {
+
+        } else {
+            alert("Preencha todos os campos")
+        }
+    }
+
     return (
-        <div>Home</div>
+        <div className='home-container'>
+            <h1>Lista de tarefas</h1>
+            <span>Gerencia sua agenda de forma fácil</span>
+
+            <form className='form' onSubmit={handleLogin}>
+                <input
+                    type="text"
+                    placeholder='Digite seu email...'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)} />
+                <input
+                    type="password"
+                    placeholder='**********'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    autoComplete={false} />
+
+                <button type='submit'>Acessar</button>
+            </form>
+
+            <Link to={'/register'} className='btn-link'>Não possui uma conta? Cadastre-se</Link>
+        </div>
     )
 }
 
