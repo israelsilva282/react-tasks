ce712eb605032f1cca3eabb723b46c8b32eeba06
israelsilva282 <israelcostas@hotmail.com>
2023-08-04 17:29:39 -0300

    Privando rota admin

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Admin from "../pages/Admin";
 import Home from "../pages/Home";
 import Register from "../pages/Register";
+import Private from "./Private";
 
 export function RoutesApp() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/admin" element={<Admin />} />
+            <Route path="/admin" element={<Private><Admin /></Private>} />
         </Routes>
     )
 }
